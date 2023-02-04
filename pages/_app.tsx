1067d86f9e005c791c7c0d234f65caac4a6a31b3
devmonkna <samaneranateechai@gmail.com>
2023-02-04 17:16:59 +0700

    updata default font:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,9 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
-import { Roboto, Kanit, Mali, Imprima } from '@next/font/google';
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: '500',
-});
-
-const kanit = Kanit({
-  weight: ['300'],
-  subsets: ['thai', 'latin'],
-});
-
-const mali = Mali({
-  weight: ['400'],
-  subsets: ['thai', 'latin'],
-});
-
-const imprima = Imprima({
-  weight: ['400'],
-  subsets: ['latin'],
-});
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${kanit.className}`}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
